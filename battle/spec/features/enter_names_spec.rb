7b8efefd893e4a9ed73e879cfa4c6d7e96665ee1
Valeria Serci <balerty84@gmail.com>
2019-09-06 11:24:56 +0100

    Test Helpers

--- battle/spec/features/enter_names_spec.rb
+++ battle/spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
-  scenario 'Players can submit names' do
-    visit '/'
-    fill_in :player_1_name, with: 'Vale'
-    fill_in :player_2_name, with: 'Bob'
-    click_button 'Submit'
+  scenario 'Submitting names' do
+    sign_in_and_play
     expect(page).to have_content 'Vale vs. Bob'
   end
 end
